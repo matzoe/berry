be3d2335df4c1811164f01254e9540630c88892d
Markus Tzoe <teamo@live.com>
2014-01-24 23:13:28 +0800

    add my second module testing: HC-SR04

---
+++ modules/ultrasonic/HC-SR04/README.md
@@ -0,0 +1,17 @@
+# HC-SR04 Ultrasonic Sensor
+
+![Front](https://github.com/matzoe/berry/raw/master/modules/ultrasonic/HC-SR04/project1.jpg)
+
+![Back](https://github.com/matzoe/berry/raw/master/modules/ultrasonic/HC-SR04/project2.jpg)
+
+## Manual
+
+[Spec](https://docs.google.com/document/d/11P_yApT0GOQx0r7L9P7xpdgvFy2y32uv2gTFaWLa9q0)
+
+
+## References
+
+- [Using an Ultrasonic Sensor (HC-SR04) on a Raspberry Pi with Python](http://www.bytecreation.com/blog/2013/10/13/raspberry-pi-ultrasonic-sensor-hc-sr04)
+- [Ultrasonic Distance Measurement Using Python – Part 1](http://www.raspberrypi-spy.co.uk/2012/12/ultrasonic-distance-measurement-using-python-part-1/)
+- [Ultrasonic Distance Measurement Using Python – Part 2](http://www.raspberrypi-spy.co.uk/2013/01/ultrasonic-distance-measurement-using-python-part-2/)
+
---
+++ modules/ultrasonic/HC-SR04/ultrasonic_1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|R|a|s|p|b|e|r|r|y|P|i|-|S|p|y|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#
+# ultrasonic_1.py
+# Measure distance using an ultrasonic module
+#
+# Author : Matt Hawkins
+# Date   : 09/01/2013
+
+# Import required Python libraries
+import time
+import RPi.GPIO as GPIO
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_TRIGGER = 17
+GPIO_ECHO    = 27
+
+print "Ultrasonic Measurement"
+
+# Set pins as output and input
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+
+# Set trigger to False (Low)
+GPIO.output(GPIO_TRIGGER, False)
+
+# Allow module to settle
+time.sleep(0.5)
+
+# Send 10us pulse to trigger
+GPIO.output(GPIO_TRIGGER, True)
+time.sleep(0.00001)
+GPIO.output(GPIO_TRIGGER, False)
+start = time.time()
+
+while GPIO.input(GPIO_ECHO)==0:
+  start = time.time()
+
+while GPIO.input(GPIO_ECHO)==1:
+  stop = time.time()
+
+# Calculate pulse length
+elapsed = stop-start
+
+# Distance pulse travelled in that time is time
+# multiplied by the speed of sound (cm/s)
+distance = elapsed * 34300
+
+# That was the distance there and back so halve the value
+distance = distance / 2
+
+print "Distance : %.1f" % distance
+
+# Reset GPIO settings
+GPIO.cleanup()
+
